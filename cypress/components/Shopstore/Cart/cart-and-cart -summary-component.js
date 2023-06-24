33c81b12392b0892544a6814d9183bd182ffdae6
Kimberling Yarihuaman <kimberling.yarihuaman@gmail.com>
2023-06-23 23:14:36 -0400

    Adding the test cases for cart and card summary page

--- cypress/components/Shopstore/Cart/cart-and-cart -summary-component.js
+++ cypress/components/Shopstore/Cart/cart-and-cart -summary-component.js
@@ -1,5 +1,20 @@
 export const cartSummary = {
     getCartInformation: () => {
         return cy.get('[id="cart_info_table"]')
+    },
+    getTableHeader: () => {
+        return cy.get('.cart_menu').children()
+    },
+    getTableBody: () => {
+        return cy.get('tbody>tr').children()
+    },
+    getProductLink: () => {
+        return cy.get('h4').children()
+    },
+    getDeleteButton: () => {
+        return cy.get('.cart_quantity_delete')
+    },
+    getCheckOutButton: () => {
+        return cy.get('.btn.btn-default.check_out')
     }
 }
--- cypress/e2e/Shopstore/Cart/cart_and_cart _summary.spec.js
+++ cypress/e2e/Shopstore/Cart/cart_and_cart _summary.spec.js
@@ -1,4 +1,5 @@
 import { cartSummary } from '../../../components/Shopstore/Cart/cart-and-cart -summary-component';
+import { modal } from '../../../components/Common/modal-component';
 
 describe('Cart And Cart Summary Tests', () => {
 
@@ -10,5 +11,56 @@ describe('Cart And Cart Summary Tests', () => {
     it('[TXX] - User Is Able To See The Cart Summary', function () {
         //GET THE CART INFORMATION
         cartSummary.getCartInformation().should('be.visible')
+
+        //VALIDATE THE TABLE HEADER
+        cartSummary.getTableHeader().should('be.visible')
+        .its('length').should('eq', 6)
+
+        //VALIDATE THE TABLE BODY
+        cartSummary.getTableBody().should('be.visible')
+        .its('length').should('eq', 6)
+
+        //VALIDATE PRODUCT LINK
+        cartSummary.getProductLink().should('be.visible')
+        .and("have.attr", "href").then((href) => {
+            cy.pageReturnValidStatus(href);
+        })
+        //CHECKOUT BUTTON
+        cartSummary.getCheckOutButton().should('be.visible')
+
+        //DELETE BUTTON
+        cartSummary.getDeleteButton().should('be.visible')
+    })
+
+    it('[TXX] - User Is Able To Click On The Delete Button', function () {
+        //CLIK ON THE DELETE BUTTON
+        cartSummary.getDeleteButton().click({ force: true })
+
+        //THE TABLE INFORMATION IS NOT VISIBLE
+        cartSummary.getCartInformation().should('not.be.visible')
+    })
+
+    it.only('[TXX] - User Is Able To Click On The Proceed To Checkout Button', function () {
+        //CLIK ON THE PROCEED TO CHECKOUT BUTTON
+        cartSummary.getCheckOutButton().click({ force: true })
+
+         //SEE THE CHECKOUT MODAL
+         modal.getModal().should('be.visible').within(() =>{
+            //HEADER
+            modal.getModalHeader().should('be.visible')
+            modal.getModalHeaderIcon().should('be.visible')
+
+            //BODY
+            modal.getModalBody().should('be.visible')
+            modal.getModalText().should('be.visible')
+            modal.getModalLink().should('be.visible')
+            .and("have.attr", "href").then((href) => {
+                cy.pageReturnValidStatus(href);
+            })
+
+            //FOOTER
+            modal.getModalFooter().should('be.visible')
+            modal.getModalButton().should('be.visible')
+        })
     })
 })
